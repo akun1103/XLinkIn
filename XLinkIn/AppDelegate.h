f25d6345c276ae93d0e681b241bb50ff3503b083
Yin Kevin <kevin@kevintekimac-mini.local>
2016-04-03 14:27:28 +0800

    Initial Commit

---
+++ XLinkIn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XLinkIn
+//
+//  Created by Kevin Yin on 16/4/3.
+//  Copyright © 2016年 Kevin Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
